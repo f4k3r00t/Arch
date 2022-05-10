1b39bc29e8c59fcded886f456303fc8952736d8a
John <104370073+f4k3r00t@users.noreply.github.com>
2022-05-10 11:31:05 +0000

    Update pacman-packages.sh

--- pacman-packages.sh
+++ pacman-packages.sh
@@ -6,43 +6,43 @@ whereami=$(pwd)
 
 echo "Hello! You are logged in as $user and you are currently in $whereami. Today is $date"
 
-sleep 1
+sleep 2
 
 echo "this script will install all packages"
 
-sleep 1
+sleep 2
 
 sudo apt pacman -Syyu
 
-sleep 1
+sleep 2
 
-sudo pacman -S –noconfirm vlc
-sudo pacman -S –noconfirm python3-pip
-sudo pacman -S –noconfirm git 
-sudo pacman -S –noconfirm vim 
-sudo pacman -S –noconfirm youtube-dl 
-sudo pacman -S –noconfirm torbrowser-launcher 
-sudo pacman -S –noconfirm tor 
-sudo pacman -S –noconfirm proxychains4 
-sudo pacman -S –noconfirm keepassxc 
-sudo pacman -S –noconfirm font-hack 
-sudo pacman -S –noconfirm ristretto 
-sudo pacman -S –noconfirm gallery-dl 
-sudo pacman -S –noconfirm gufw 
-sudo pacman -S –noconfirm bleachbit 
-sudo pacman -S –noconfirm atril 
-sudo pacman -S –noconfirm gparted 
-sudo pacman -S –noconfirm mousepad 
-sudo pacman -S –noconfirm htop 
-sudo pacman -S –noconfirm btop 
-sudo pacman -S –noconfirm neofetch 
-sudo pacman -S –noconfirm xfce4-terminal 
-sudo pacman -S –noconfirm rkhunter 
-sudo pacman -S –noconfirm chkrootkit 
-sudo pacman -S –noconfirm lynis 
-sudo pacman -S –noconfirm clipgrab
+sudo pacman -S vlc
+sudo pacman -S python3-pip
+sudo pacman -S git 
+sudo pacman -S vim 
+sudo pacman -S youtube-dl 
+sudo pacman -S torbrowser-launcher 
+sudo pacman -S tor 
+sudo pacman -S proxychains4 
+sudo pacman -S keepassxc 
+sudo pacman -S font-hack 
+sudo pacman -S ristretto 
+sudo pacman -S gallery-dl 
+sudo pacman -S gufw 
+sudo pacman -S bleachbit 
+sudo pacman -S atril 
+sudo pacman -S gparted 
+sudo pacman -S mousepad 
+sudo pacman -S htop 
+sudo pacman -S btop 
+sudo pacman -S neofetch 
+sudo pacman -S xfce4-terminal 
+sudo pacman -S rkhunter 
+sudo pacman -S chkrootkit 
+sudo pacman -S lynis 
+sudo pacman -S clipgrab
 
-sleep 1
+sleep 2
 
 echo '#######################################################'
 echo '################## INSTALLATION DONE ##################'
